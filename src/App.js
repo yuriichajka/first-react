58496960c07502a485bdf6b5604f3d37a18201e7
yuriichajka <yuriichajka@gmail.com>
2021-07-06 10:50:05 +0300

    redux homework part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useSelector, useDispatch} from "react-redux";
+
+const SomeNestedChildComponent = () => {
+
+    const counter = useSelector(({counterValue}) => counterValue);
+    console.log(counter)
+
+
+    return (
+        <div>
+            <h1>{counter}</h1>
+
+        </div>
+    )
+}
+
+
+const SomeChildComponent = () => {
+
+    return (
+       <div>
+           <SomeNestedChildComponent/>
+       </div>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch()
+
+
+    return (
+        <div>
+            <button onClick={() => {
+                dispatch({type: 'INC'})
+            }}>inc</button>
+
+            <SomeChildComponent/>
+
+            <button onClick={() => {
+                dispatch({type: 'DEC'})
+            }}>dec</button>
+
+            <button onClick={() => {
+                dispatch({type: 'RES'})
+            }}>reset</button>
+
+            <button onClick={() => {
+                dispatch({type: 'X2'})
+            }}>x2</button>
+        </div>
+    )
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,55 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialState = {
+    counterValue: 0
+}
+
+const counterReduser = (state = initialState, action) => {
+
+    // console.log({action, state})
+
+    switch (action.type) {
+        case 'INC': {
+            return {...state, counterValue: state.counterValue + 1};
+        }
+        case 'DEC': {
+            return {...state, counterValue: state.counterValue - 1};
+        }
+        case 'RES': {
+            return {...state, counterValue: 0};
+        }
+        case 'X2': {
+            return {...state, counterValue: state.counterValue * 2};
+        }
+        default: return state;
+    }
+}
+
+const store = createStore(counterReduser)
+
+// console.log(store)
+//
+// console.log(store.getState())
+
+// store.subscribe(() => {
+//     console.log('from subscribe', store.getState())
+// })
+//
+// store.dispatch({
+//     type: 'INC'
+// })
+
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
